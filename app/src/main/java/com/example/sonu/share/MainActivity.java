cbfb14508211341944f07256dfd6c32a9c32a4d1
rashmi1309 <raashmivenkatesh13@gmail.com>
2019-07-09 20:55:41 +0530

    Share_screen
    code of share screen

---
+++ app/src/main/java/com/example/sonu/share/MainActivity.java
@@ -0,0 +1,24 @@
+
+package com.example.sonu.share;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "INVITE FRIENDS");
+        adapter.addFragment(new Tab2Fragment(), "CREDITS EARNED");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
